e311faef4870b0dafcd5e7e0348fc62dd9bbc867
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-02-11 21:10:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"sultankhilji001@gmail.com\"n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
