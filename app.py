19134cdfcea6b539fe764327cd2969e1ba771c62
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-02-13 11:13:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def pencilsketch(inp_img):
     img_gray = cv2.cvtColor(inp_img, cv2.COLOR_BGR2GRAY)
     img_invert = cv2.bitwise_not(img_gray)
     img_smoothing = cv2.GaussianBlur(img_invert, (21, 21),sigmaX=0, sigmaY=0)
-    final_img = dodgeV2(img_gray, img_smoothing)
+    final_img = dodgeV2(img_gray, img_smoothing)-40
     return(final_img)
 
 
